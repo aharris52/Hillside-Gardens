f675be18d9571fb0a857e92b971e8cac1929caf7
aharris52 <aharris@mail.greenriver.edu>
2020-06-09 15:12:21 -0400

    added controller

---
+++ controller/controller.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Controller
+{
+    private $_f3; //router
+    private $_validation; //validation object
+
+    /**
+     * Controller constructor.
+     * @param $f3
+     * @param $validation
+     */
+    public function __construct($f3, $validation)
+    {
+        $this->_f3 = $f3;
+        $this->_validation = $validation;
+    }
+
+    /**
+     * Display the default route
+     */
+    public function home()
+    {
+        echo '<h1>Welcome to my Home Page</h1>';
+        $view = new Template();
+        echo $view->render('views/demo-01.html');
+    }
+
+}
