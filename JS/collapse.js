e76deaf6eab7bfc2d3269c5388ada5254f5a2e98
Benjamin Halbert <bhalbert2@mail.greenriver.edu>
2020-06-10 15:42:22 -0400

    code to collapse and reshow

---
+++ JS/collapse.js
@@ -0,0 +1,22 @@
+let check = false;
+
+function btncheck(){
+    check = true;
+}
+
+if(check === true){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}
