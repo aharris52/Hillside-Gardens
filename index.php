b47be105e91f203e313634c67aaf7f977e9f9e74
aharris52 <aharris@mail.greenriver.edu>
2020-06-12 20:04:01 -0400

    moved form handling to controller

--- index.php
+++ index.php
@@ -17,91 +17,19 @@ $f3 = Base::instance();
 global $validation;
 $validation = new Validation();
 global $controller;
-//$controller = new Controller($f3, $validation);
 
+//Instantiate my classes
+$f3 = Base::instance();
+$validator = new Validation();
+$controller = new Controller($f3, $validator);
+$db = new Database();
 
 // define a default route
 $f3->route('GET /', function($f3) {
     $view = new template();
     echo $view->render('views/demo-01.html');
-
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if (!validName($_POST['firstName'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["firstName"]', "Invalid First Name.");
-        }
-        if (!validName($_POST['lastName'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["lastName"]', "Invalid Last Name.");
-        }
-        if (!validPhone($_POST['phone'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["phone"]', "Invalid Phone Number.");
-        }
-        if (!validEmail($_POST['email'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["email"]', "Invalid Email.");
-        }
-        if (!validProduct($_POST['products'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["Products"]', "Invalid Products.");
-        }
-        if (!validQuantity($_POST['quantity'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["quantity"]', "Invalid Quantity");
-        }
-
-        if(empty($f3->GET('errors'))){
-            $fName = $_POST['firstName'];
-            $lName = $_POST['lastName'];
-            $phone = $_POST['phone'];
-            $email = $_POST['email'];
-            $product = $_POST['products'];
-            $qty = $_POST['quantity'];
-
-            //connect to db
-            require('/home/aharrisg/config.php');
-            try {
-                //Instantiate a database object
-                $dbh = new PDO (DB_DSN, DB_USERNAME, DB_PASSOWRD);
-                echo "Connected to database!";
-            }
-            catch(PDOException $e) {
-                echo $e->getMessage();
-            }
-
-            //define query
-            $sql_customer = "INSERT INTO `Customer` VALUES ('NULL',:fName, :lName, :phone, :email)";
-            $sql_order = "INSERT INTO `Order` VALUES ('NULL', :cust_foreign, :prod_foreign, :qty, 'NULL', 'NULL')";
-
-            //prepare statement
-            $statement_customer =  $dbh->prepare($sql_customer);
-            $statement_order =  $dbh->prepare($sql_order);
-
-            //bind parameters
-            $statement_customer->bindParam(':fName', $fName, PDO::PARAM_STR);
-            $statement_customer->bindParam(':lName', $lName, PDO::PARAM_STR);
-            $statement_customer->bindParam(':phone', $phone, PDO::PARAM_STR);
-            $statement_customer->bindParam(':email', $email, PDO::PARAM_STR);
-            //$statement->bindParam(':product', $product, PDO::PARAM_STR);
-            $statement_order->bindParam(':qty', $qty, PDO::PARAM_STR);
-
-            //Execute the statement
-            $statement_customer->execute();
-            $statement_order->execute();
-
-            //process the result
-            $row = $statement_customer->fetch(PDO::FETCH_ASSOC);
-            $row = $statement_order->fetch(PDO::FETCH_ASSOC);
-            //echo $row['fname'];
-        }
-    }
+    echo("this is a test");
 });
+
 $f3->run();
 
